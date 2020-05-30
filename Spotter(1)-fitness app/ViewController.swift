e5ad9f79396692a3a51cbac0e43c7dd6714064eb
Me <wwwierschem@icloud.com>
2020-05-30 16:17:54 -0400

    Initial Commit

---
+++ Spotter(1)-fitness app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotter(1)-fitness app
+//
+//  Created by Pierce Wierschem on 5/30/20.
+//  Copyright © 2020 Pierce Wierschem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Spotter(1)-fitness appTests/Spotter_1__fitness_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  Spotter_1__fitness_appTests.swift
+//  Spotter(1)-fitness appTests
+//
+//  Created by Pierce Wierschem on 5/30/20.
+//  Copyright © 2020 Pierce Wierschem. All rights reserved.
+//
+
+import XCTest
+@testable import Spotter_1__fitness_app
+
+class Spotter_1__fitness_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
